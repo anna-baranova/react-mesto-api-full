7e520697a71334984e06c567f1e5deef90a5f4d7
Anna Baranova <baranova.a.i@yandex.ru>
2021-12-09 10:07:01 +0300

    changed URL in frontend

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'mestoproject.nomoredomains.work/';
 
 // Регистрация / авторизация пользователя
 const authApi = (password, email, path) => {
