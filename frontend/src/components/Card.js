c8a549b73d51f24f00b2c107b0ac04a7e52454fc
Anna Baranova <baranova.a.i@yandex.ru>
2021-12-10 16:28:22 +0300

    12

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,7 +4,7 @@ import CurrentUserContext from '../contexts/CurrentUserContext'
 function Card(props) {
     const currentUser = React.useContext(CurrentUserContext);
     // Определяем, являемся ли мы владельцем текущей карточки
-    const isOwn = props.card.owner._id === currentUser._id;
+    const isOwn = props.card.owner === currentUser._id;
     // Создаём переменную, которую после зададим в `className` для кнопки удаления
     const cardDeleteButtonClassName = (
         `place-grid__delete ${isOwn ? '' : 'place-grid__delete_hidden'}`
