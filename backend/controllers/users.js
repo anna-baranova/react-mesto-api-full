f79a2ee9588ab7f7466d2ab88bdb65781ff80075
Anna Baranova <baranova.a.i@yandex.ru>
2021-12-10 14:02:19 +0300

    5

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -104,7 +104,7 @@ const login = (req, res, next) => {
         sameSite: 'None',
         secure: true,
       })
-        .status(200).send(token);
+        .status(200).send({ token });
     })
     .catch(() => {
       throw new UnauthorizedError('Необходима авторизация');
