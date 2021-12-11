89244903536a3d42c6fa9f5c1cb7d1fbb792bd2a
Anna Baranova <baranova.a.i@yandex.ru>
2021-12-11 18:31:24 +0300

    15

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -11,7 +11,7 @@ function Card(props) {
     ); 
 
     // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
-    const isLiked = props.card.likes.some(i => i._id === currentUser._id);
+    const isLiked = props.card.likes.some(i => i === currentUser._id);
     // Создаём переменную, которую после зададим в `className` для кнопки лайка
     const cardLikeButtonClassName = (
         `place-grid__like-btn ${isLiked ? 'place-grid__like-liked' : ''}`
