7b5ceec0c22a6da090b576837020135674621f3c
Anna Baranova <baranova.a.i@yandex.ru>
2021-12-12 13:52:37 +0300

    23

--- backend/app.js
+++ backend/app.js
@@ -41,6 +41,12 @@ app.use(cors({
 app.use(express.json());
 app.use(requestLogger); // подключаем логгер запросов
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post(
   '/signin',
   celebrate({
